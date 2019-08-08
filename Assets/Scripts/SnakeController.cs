fc6133d105e73905fe4d26a7b100604963cf412c
Carson <bursttat@gmail.com>
2019-08-08 18:03:36 +1000

    Energy Bar and Health Bar
    Added Energy Bar for accelerating and Health Bar.

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SnakeController : MonoBehaviour
 {
     private int length;
+    private Slider energyBar;
+    private Slider healthBar;
     private LevelController lvControl;
     private GameObject bodyPrefab;
     private Transform allBody;
     private Transform firstBody;
-    private float speed;
+    private float movingSpeed;
+    private float steeringSpeed;
+    private float energy;
 
     private Rigidbody2D rigi;
     float timer;
@@ -18,12 +22,17 @@ public class SnakeController : MonoBehaviour
     void Start()
     {
         lvControl = GameObject.Find("LevelController").GetComponent<LevelController>();
-        speed = lvControl.speed;
+        movingSpeed = lvControl.speed;
+        steeringSpeed = 10.0f;
         bodyPrefab = Resources.Load<GameObject>("Prefabs/Body");
         allBody = GameObject.Find("SnakeBody").transform;
         rigi = transform.GetComponent<Rigidbody2D>();
         length = 20;
-        for(int n = 0; n < length; n++)
+        energy = 100;
+        energyBar = GameObject.Find("EnergyBar").GetComponent<Slider>(); 
+        healthBar = GameObject.Find("HealthBar").GetComponent<Slider>();
+
+        for (int n = 0; n < length; n++)
         {
             GameObject newBody =  Instantiate(bodyPrefab,allBody);
             newBody.transform.position = new Vector2(transform.position.x, transform.position.y);
@@ -36,7 +45,8 @@ public class SnakeController : MonoBehaviour
     void FixedUpdate()
     {
 
-        rigi.velocity = transform.up*speed;
+
+        rigi.velocity = transform.up*movingSpeed;
         //firstBody.position = transform.position;
         firstBody.GetComponent<Rigidbody2D>().velocity = rigi.velocity;
         for (int n = length-1; n > 0; n--)
@@ -44,6 +54,8 @@ public class SnakeController : MonoBehaviour
             //allBody.GetChild(n).transform.position =allBody.GetChild(n-1).transform.position;
             allBody.GetChild(n).GetComponent<Rigidbody2D>().velocity = allBody.GetChild(n - 1).GetComponent<Rigidbody2D>().velocity;
         }
+        energyBar.value = energy;
+        AbilitiesDetection();
         MovementDetection();
       
     }
@@ -56,7 +68,35 @@ public class SnakeController : MonoBehaviour
             newBody.transform.position = new Vector2(allBody.GetChild(length-1).position.x, allBody.GetChild(length - 1).position.y);
         }
         length += 5;
+    }
 
+    private void RecoverEnergy(float value)
+    {
+        energy += value;
+    }
+
+    private void AbilitiesDetection()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (energy >= 10.0f)
+            {
+                movingSpeed = 20.0f;
+                steeringSpeed = 10.0f;
+                energy -= 0.1f;
+            }
+            else
+            {
+                movingSpeed = 10.0f;
+                steeringSpeed = 10.0f;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            movingSpeed = 10.0f;
+            steeringSpeed = 10.0f;
+        }
     }
 
     private void MovementDetection()
@@ -66,7 +106,7 @@ public class SnakeController : MonoBehaviour
             timer += Time.fixedDeltaTime;
             if (timer <= 0.5f)
             {
-                transform.Rotate(0, 0, 10 * timer);
+                transform.Rotate(0, 0, steeringSpeed * timer);
             }
             else
             {
@@ -80,7 +120,7 @@ public class SnakeController : MonoBehaviour
             timer += Time.fixedDeltaTime;
             if (timer <= 0.5f)
             {
-                transform.Rotate(0, 0, -10 * timer);
+                transform.Rotate(0, 0, -steeringSpeed * timer);
             }
             else
             {
