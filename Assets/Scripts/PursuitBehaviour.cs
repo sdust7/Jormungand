19caf306a520b7a4e97eac407d2614b5a17a9e1c
DESKTOP-FE8M3A8\Doubl <s3660255@student.rmit.edu.au>
2019-09-09 19:28:09 +1000

    Added A* pathfinding and started working on converting other projects pathfinding into this project. A* grid working well, wolf and its ai aren't working at all yet

---
+++ Assets/Scripts/PursuitBehaviour.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+
+public class PursuitBehaviour : MonoBehaviour
+{
+    [SerializeField] protected GameObject targetObj;
+    private Vector3 target;
+    [SerializeField] private float predictionTime;
+    private float predictionTimeBase;
+    [SerializeField] protected float arrivalRadius;
+
+    [SerializeField] private float nextWayPointDist;
+
+    Path path;
+    int currentWayPoint = 0;
+    bool reachedEnd = false;
+
+    Seeker seeker;
+    public Rigidbody2D rigidBod;
+
+    private CircleCollider2D circleCollider;
+    private Rigidbody2D targetRigidBody;
+
+    private void Awake()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    private void Start()
+    {
+        predictionTimeBase = predictionTime;
+        if (targetObj == null)
+        {
+            target = transform.position;
+        }
+        else
+        {
+            targetRigidBody = targetObj.GetComponent<Rigidbody2D>();
+        }
+        seeker = GetComponent<Seeker>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rigidBod.position, target, OnPathCompelte);
+        }
+    }
+
+    void OnPathCompelte(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+
+    }
+
+    public void ApplySteering(Rigidbody2D rb)
+    {
+        // Return if no target
+        if (targetObj == null)
+            return;
+
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            reachedEnd = true;
+            return;
+        }
+        else
+        {
+            reachedEnd = false;
+        }     
+    }
+
+    public void SetTargetObj(GameObject targetObj)
+    {
+        this.targetObj = targetObj;
+        targetRigidBody = targetObj.GetComponent<Rigidbody2D>();
+    }
+
+    public bool ToggleHunger(bool full)
+    {
+        if (full)
+        {
+            full = false;
+        }
+        else
+        {
+            full = true;
+        }
+        return full;
+    }
+}
---
+++ Assets/Scripts/WolfAI.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI : MonoBehaviour
+{
+    public enum WolfState { Wandering, Hunting, Eating };
+
+    private PursuitBehaviour pursuit;
+    protected GameObject targetObj;
+
+    public WolfState state = WolfState.Wandering;
+    private CircleCollider2D circleCollider;
+    public Rigidbody2D rb; // IMPORTANT: Set this to the parent if AI is in a child object
+    private bool full;
+    private bool runningCoroutine = false;
+
+    void Awake()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        pursuit = GetComponent<PursuitBehaviour>();
+    }
+
+    IEnumerator eatingFull()
+    {
+        //Toggle bool to prevent multiple instances of coroutine running at the same time
+        runningCoroutine = true;
+        yield return new WaitForSeconds(3);
+        full = pursuit.ToggleHunger(full);
+        runningCoroutine = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (targetObj == this.gameObject)
+        {
+            state = WolfState.Wandering;
+        }
+        else if (full)
+        {
+            state = WolfState.Eating;
+        }
+        else
+        {
+            state = WolfState.Hunting;
+        }
+
+        pursuit.ApplySteering(rb);
+
+        //check if already running coroutine
+        if (runningCoroutine != true)
+        {
+            StartCoroutine(eatingFull());
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Track fish and player if nearby
+        if (collision != null && (collision.gameObject.layer == 9 || collision.gameObject.name == "Player"))
+        {
+            if (targetObj == this.gameObject || collision.gameObject.name == "Player")
+            {
+                targetObj = collision.gameObject;
+                pursuit.SetTargetObj(targetObj);
+            }
+        }
+        //Track self as a substitute for null targets
+        else
+        {
+            targetObj = this.gameObject;
+            pursuit.SetTargetObj(targetObj);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (full)
+            return;
+
+        //Delete fish if not full
+        if (collision != null && collision.gameObject.layer == 9)
+        {
+            full = pursuit.ToggleHunger(full);
+            Destroy(collision.gameObject);
+        }
+        //Delete player if not full
+        else if (collision != null && collision.gameObject.layer == 8)
+        {
+            full = pursuit.ToggleHunger(full);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void SetState(int stateNumber)
+    {
+        state = (WolfState)stateNumber;
+    }
+}
